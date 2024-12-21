278ee2bbafff4cfb0ab6a66ac57c83e90bbfb14b
FernandoCassioDev <fernandocassiodev@gmail.com>
2024-12-21 14:44:36 -0300

    feat: building the introducting of the HomePage

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+
+import Navbar from "@/app/components/navbar/Navbar";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <Navbar />
+      <div className="my-7 mx-3">
+        <h1 className="font-extrabold text-2xl">
+          Hello 👋
+          <br /> I am Fernando a professional
+          {/* add typing animation at final part of the text */}
+        </h1>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
