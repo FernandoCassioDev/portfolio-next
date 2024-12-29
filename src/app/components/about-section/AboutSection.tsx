3e3b68982437853708ba5ddcb78188bcc282604f
Fernando <fernandocassiodev@gmail.com>
2024-12-28 22:42:43 -0300

    feat: AboutSection component created

---
+++ src/app/components/about-section/AboutSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AboutSection() {
+  return (
+    <div>
+      <h2 className="text-3xl text-white  text-center">
+        ABOUT ME
+      </h2>
+    </div>
+  );
+}
+
+export default AboutSection;
