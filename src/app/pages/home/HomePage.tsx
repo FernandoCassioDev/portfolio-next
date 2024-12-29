d90f80ded929be6a30d256756ad64e29cc1ecc47
Fernando <fernandocassiodev@gmail.com>
2024-12-28 22:41:36 -0300

    refactor: semantic of HTML improved

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -8,28 +8,30 @@ import { Typewriter } from "react-simple-typewriter";
 function HomePage() {
   return (
     <>
-      <Navbar />
-      <div className="my-7 mx-3">
-        <h1 className="font-extrabold text-2xl">
-          Hello 👋
-          <br />I am Fernando a professional
-        </h1>
-        <div className="text-green-500 font-extrabold text-2xl h-8">
-            <Typewriter
-              words={["Software Engineer"]}
-              loop={false} // Number of loops (set to `false` for infinite)
-              typeSpeed={100} // Typing speed
-              deleteSpeed={50} // Deleting speed
-              delaySpeed={1000} // Delay between words
-            />
-          </div>
-      </div>
-      <div>
+      <section>
+        <Navbar />
+        <div className="my-7 mx-3">
+          <h1 className="font-extrabold text-2xl">
+            Hello 👋
+            <br />I am Fernando a professional
+          </h1>
+          <div className="text-green-500 font-extrabold text-2xl h-8">
+              <Typewriter
+                words={["Software Engineer"]}
+                loop={false} // Number of loops (set to `false` for infinite)
+                typeSpeed={100} // Typing speed
+                deleteSpeed={50} // Deleting speed
+                delaySpeed={1000} // Delay between words
+              />
+            </div>
+        </div>
+      </section>
+      <section>
         <CodeDesign />
-      </div>
-      <div>
+      </section>
+      <section>
         <TechCarousel />
-      </div>
+      </section>
     </>
   );
 }
